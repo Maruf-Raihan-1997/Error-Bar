a0a0c466f0ca7844ac967c1a3b11df02d2fddf52
Maruf-Raihan-1997 <maruf.nstu101@gmail.com>
2025-08-14 19:05:58 +0300

    Plyr
    Plyr package
    
    Creating a function for summrizing data based on grouping variable

--- error bar.R
+++ error bar.R
@@ -1,1 +1,24 @@
-#
+#searching dataset
+
+ToothGrowth
+Tooth_data= ToothGrowth
+
+#Creating a function for summarizing data
+#based on grouping variable
+
+
+data_summary = function (data , varname, groupnames){
+  require(plyr)
+  
+  summary_func = function (x, col){
+    c(mean= mean(x[[col]]), na.rm= TRUE,
+    sd= sd(x[[col]]),na.rm= TRUE) 
+  }
+      data_sum = ddply(data, groupnames, .fun= summary_func, varname)
+      data_sum= rename (data_sum, c("mean"= varname))
+    return(data_sum)}
+
+
+d_summary= data_summary(Tooth_data, varname= "len", 
+                        groupnames= c("supp", "dose"))
+
