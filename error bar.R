cf903b53b59eb9df8c2b0cc93076b5447369896f
Maruf-Raihan-1997 <maruf.nstu101@gmail.com>
2025-08-15 18:05:56 +0300

    Update error bar.R

--- error bar.R
+++ error bar.R
@@ -11,8 +11,7 @@ data_summary = function (data , varname, groupnames){
   require(plyr)
   
   summary_func = function (x, col){
-    c(mean= mean(x[[col]]), na.rm= TRUE,
-    sd= sd(x[[col]]),na.rm= TRUE) 
+    c(mean= mean(x[[col]]), na.rm= TRUE, sd   = sd(x[[col]],   na.rm = TRUE)) 
   }
       data_sum = ddply(data, groupnames, .fun= summary_func, varname)
       data_sum= rename (data_sum, c("mean"= varname))
@@ -21,4 +20,22 @@ data_summary = function (data , varname, groupnames){
 
 d_summary= data_summary(Tooth_data, varname= "len", 
                         groupnames= c("supp", "dose"))
+
+
+#converting dose to a factor
+
+d_summary$dose = as.factor(d_summary$dose)
+
+
+class(d_summary$dose)
+
+#SD of the mean as error bar with explanation
+
+library(ggplot2)
+
+ggplot(d_summary, aes(x=dose, y= len, fill = supp))+
+  geom_bar(stat="identity", position = position_dodge())+
+  geom_errorbar(aes(ymin = len- sd ,ymax = len + sd ),width = 0.2, position = position_dodge(.8))+
+  theme_minimal()
+
 
